0a8adbf06a799e65583bf0b794f7c18a5932a669
Geerten Vink <maedaros@yahoo.com>
2020-11-14 16:39:06 +0100

    very basic room

---
+++ World/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RunicMagic.World
+{
+    public class Room : IRoom
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<IMobile> Entities { get; set; }
+
+        public Room(string name, string description)
+        {
+            this.Name = name;
+            this.Description = description;
+
+            this.Entities = new List<IMobile>();
+        }
+    }
+}
