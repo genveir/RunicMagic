c8560f8bafe9b8c1e9cd999cace7fec9c1edcf2f
Geerten Vink <maedaros@yahoo.com>
2022-01-14 19:55:23 +0100

    remove unused tick var

--- Engine/WorldRunner.cs
+++ Engine/WorldRunner.cs
@@ -19,14 +19,12 @@ namespace Engine
 
         public int tickSizeInMS = 100;
 
-        private static long tick = 0;
         private async Task Loop(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
                 try
                 {
-                    tick++;
                     TimeSpan delay = TimeSpan.FromMilliseconds(tickSizeInMS);
 
                     var next = DateTime.Now.Add(delay);
